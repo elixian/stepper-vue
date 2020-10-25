b925eb905cfa30edf027a49e140c4077bce6881e
Franck Colombo <franck.colombo@cnav.fr>
2020-10-25 22:06:45 +0100

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import StepperVue from '@/stepper-vue.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    StepperVue
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <stepper-vue :active-page="2" />
+  </div>
+</template>
---
+++ src/stepper-vue.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div id="stepper" :style="{ width: percentStage + '%' }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: 
+    {
+      activePage: {
+        type: Number,
+        default: 1,
+      },
+      totalPage: {
+        type: Number,
+        default:3,
+      },
+    },
+  computed:{
+    percentStage(){
+      return (this.activePage * 100) / this.totalPage;
+    }
+  }
+
+};
+</script>
+
+<style lang="scss" scoped>
+$primary-color: rgb(225, 68, 240);
+#stepper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 2px;
+  width: 0%;
+  background: $primary-color;
+  transition: width 1s ease-out;
+}
+
+@keyframes fluidStep {
+  100% {
+    width: 100%;
+  }
+}
+</style>
